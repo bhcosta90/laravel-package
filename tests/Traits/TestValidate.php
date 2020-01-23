ca465669f96d4dd6b4550b8b01fc28aabb8f09f7
Bruno Costa <bruno.costa@superlogica.com>
2020-01-23 13:19:54 -0300

    arrumando composer

--- tests/Traits/TestValidate.php
+++ tests/Traits/TestValidate.php
@@ -6,18 +6,19 @@ namespace BRCas\LaravelTests\Traits;
 
 use Illuminate\Foundation\Testing\TestResponse;
 
-trait TestValidate{
+trait TestValidate
+{
     protected abstract function routeStorage();
-    
+
     protected abstract function routeUpdate();
 
     protected function assertValidationInStorageAction(
         array $data,
         string $rule,
         array $params = []
-    )
-    {
+    ) {
         $response = $this->json('POST', $this->routeStorage(), $data);
+        unset($data['token']);
         $fields = array_keys($data);
         $this->assertInvationFields($response, $fields, $rule, $params);
     }
@@ -26,27 +27,27 @@ trait TestValidate{
         array $data,
         string $rule,
         array $params = []
-    )
-    {
+    ) {
         $response = $this->json('PUT', $this->routeUpdate(), $data);
+        unset($data['token']);
         $fields = array_keys($data);
         $this->assertInvationFields($response, $fields, $rule, $params);
     }
 
     protected function assertInvationFields(
-        TestResponse $response, 
+        TestResponse $response,
         array $fields,
         string $rule,
-        array $params = [])
-    {
-        if($response->status() != 422){
+        array $params = []
+    ) {
+        if ($response->status() != 422) {
             throw new \Exception("Response status must be 201, given {$response->status()}\n{$response->content()}");
         }
 
         $response->assertStatus(422)
             ->assertJsonValidationErrors($fields);
 
-        foreach($fields as $field){
+        foreach ($fields as $field) {
             $fieldName = str_replace('_', ' ', $field);
             $response->assertJsonFragment([
                 \Lang::get("validation.{$rule}", ['attribute' => $fieldName] + $params)
