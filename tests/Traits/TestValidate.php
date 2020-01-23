0a93d399996188ae3f81c8b161d83e66555a1959
Bruno Costa <bruno.costa@superlogica.com>
2020-01-23 13:10:34 -0300

    arrumando composer

--- tests/Traits/TestValidate.php
+++ tests/Traits/TestValidate.php
@@ -14,33 +14,42 @@ trait TestValidate{
     protected function assertValidationInStorageAction(
         array $data,
         string $rule,
-        array $params = []
+        array $params = [],
+        array $remove = []
     )
     {
         $response = $this->json('POST', $this->routeStorage(), $data);
         $fields = array_keys($data);
-        $this->assertInvationFields($response, $fields, $rule, $params);
+        $this->assertInvationFields($response, $fields, $rule, $params, $remove);
     }
 
     protected function assertValidationInUpdateAction(
         array $data,
         string $rule,
-        array $params = []
+        array $params = [],
+        array $remove = []
     )
     {
         $response = $this->json('PUT', $this->routeUpdate(), $data);
         $fields = array_keys($data);
-        $this->assertInvationFields($response, $fields, $rule, $params);
+        $this->assertInvationFields($response, $fields, $rule, $params, $remove);
     }
 
     protected function assertInvationFields(
         TestResponse $response, 
         array $fields,
         string $rule,
-        array $params = [])
+        array $params = [],
+        array $remove = [])
     {
         if($response->status() != 422){
             throw new \Exception("Response status must be 201, given {$response->status()}\n{$response->content()}");
+        }
+
+        foreach($remove as $x){
+            if(isset($params[$x])){
+                unset($params[$x]);
+            }
         }
 
         $response->assertStatus(422)
