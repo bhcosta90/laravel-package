bb70769701925e2344b1ac1e47e553a3b66364dc
Bruno Costa <bruno.costa@superlogica.com>
2020-01-23 13:13:47 -0300

    Revert "arrumando composer"
    This reverts commit 0a93d399996188ae3f81c8b161d83e66555a1959.

--- tests/Traits/TestValidate.php
+++ tests/Traits/TestValidate.php
@@ -14,42 +14,33 @@ trait TestValidate{
     protected function assertValidationInStorageAction(
         array $data,
         string $rule,
-        array $params = [],
-        array $remove = []
+        array $params = []
     )
     {
         $response = $this->json('POST', $this->routeStorage(), $data);
         $fields = array_keys($data);
-        $this->assertInvationFields($response, $fields, $rule, $params, $remove);
+        $this->assertInvationFields($response, $fields, $rule, $params);
     }
 
     protected function assertValidationInUpdateAction(
         array $data,
         string $rule,
-        array $params = [],
-        array $remove = []
+        array $params = []
     )
     {
         $response = $this->json('PUT', $this->routeUpdate(), $data);
         $fields = array_keys($data);
-        $this->assertInvationFields($response, $fields, $rule, $params, $remove);
+        $this->assertInvationFields($response, $fields, $rule, $params);
     }
 
     protected function assertInvationFields(
         TestResponse $response, 
         array $fields,
         string $rule,
-        array $params = [],
-        array $remove = [])
+        array $params = [])
     {
         if($response->status() != 422){
             throw new \Exception("Response status must be 201, given {$response->status()}\n{$response->content()}");
-        }
-
-        foreach($remove as $x){
-            if(isset($params[$x])){
-                unset($params[$x]);
-            }
         }
 
         $response->assertStatus(422)
