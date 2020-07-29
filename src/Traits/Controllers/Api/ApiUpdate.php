337b3d153f26ef3dfb899533328d73d9b0f3b313
Bruno Henrique da Costa <bhcosta90@gmail.com>
2020-07-29 09:01:10 -0300

    Update ApiUpdate.php

--- src/Traits/Controllers/Api/ApiUpdate.php
+++ src/Traits/Controllers/Api/ApiUpdate.php
@@ -1,44 +1,34 @@
 <?php
 
-namespace BRCas\Laravel\Traits\Controllers\Api;
+namespace BRCas\Laravel\Traits\Controllers\Controller;
 
 use BRCas\Laravel\Utils\Message;
 use Illuminate\Http\Request;
 
-trait ApiUpdate
+trait ControllerUpdate
 {
     public function getMessageUpdate()
     {
         return Message::updated();
     }
 
+    public abstract function edit();
+
     public function update(Request $request, $id)
     {
         $this->request = $request;
 
         return $this->executeAction($request, function () use ($id) {
-            $objClass = $this->model();
-
             if (method_exists($this, 'service')) {
                 $objService = call_user_func_array([$this, 'service'], []);
                 if (method_exists($objService, 'find')) {
-                    $obj = $objService::find($id);
+                   $obj = $objService::find($id);
                 }
             }
 
+            $objClass = $this->model();
             if (empty($obj)) {
                 $obj = $objClass::findOrFail($id);
-            }
-
-            $data = [
-                "status" => 200,
-                "msg" => $this->getMessageUpdate(),
-            ];
-
-            if (method_exists($this, 'route')) {
-                $data += [
-                    'route' => $this->route()
-                ];
             }
 
             $dataSend = $this->validate($this->request, $this->rulesPut());
@@ -49,41 +39,28 @@ trait ApiUpdate
                 }
             }
 
-            $resource = $this->resource();
-
             if (method_exists($this, 'service')) {
                 $objService = call_user_func_array([$this, 'service'], []);
                 if (method_exists($objService, 'put')) {
                     $obj = $objService::put($obj, $dataSend);
-
-                    if (method_exists($this, 'route')) {
-                        $this->request->session()->flash('success', $this->getMessageUpdate());
-                    }
-
-                    return $obj;
+                    $this->request->session()->flash('success', $this->getMessageUpdate());
+                    return redirect($this->route());
                 }
             }
 
             $obj->update($dataSend);
+            $obj->save();
 
-            if (method_exists($this, 'route')) {
-                $this->request->session()->flash('success', $this->getMessageUpdate());
-
-                $data += [
-                    'route' => $this->route()
-                ];
-            }
-
-            return (new $resource($obj))
-                ->additional($data)
-                ->response()
-                ->setStatusCode(200);
+            $this->request->session()->flash('success', $this->getMessageUpdate());
+            return redirect($this->route());
         });
     }
 
     protected abstract function model();
 
     protected abstract function rulesPut();
+
+    public abstract function route();
 
     protected abstract function resource();
 }
