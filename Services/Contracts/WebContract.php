8446b9b304c952713378fa53c6549bc6eb502798
Bruno Costa <bruno.costa@superlogica.com>
2021-02-02 09:10:42 -0300

    organizando

--- Services/Contracts/WebContract.php
+++ Services/Contracts/WebContract.php
@@ -6,14 +6,13 @@ namespace Costa\Package\Services\Contracts;
 
 interface WebContract
 {
-
-    public function find($id);
-
     public function index($params): array;
 
-    public function destroy($id);
+    public function find($id);
 
     public function update($id, $data);
 
     public function store($data);
+
+    public function destroy($id);
 }
