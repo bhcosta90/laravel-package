62a741fff2af1e700047ea3acca64c1449a6a498
Bruno Costa <bruno.costa@dafiti.com.br>
2023-05-27 16:36:52 -0300

    finalizado

--- src/Resources/components/card/header.blade.php
+++ src/Resources/components/card/header.blade.php
@@ -7,7 +7,7 @@
                     <button data-keep-open="{{$keepOpen}}" class='btn btn-light'>{{ __($textRegister ?? "New register") }}</button>
                     @break
                 @default
-                <a href='{{$register}}' data-keep-open={{$keepOpen}} class='btn btn-light btn-add-by-link'>{{ __($textRegister ?? "New register") }}</a>
+                <a href='{{$register}}' data-keep-open="{{$keepOpen}}" class='btn btn-light btn-add-by-link'>{{ __($textRegister ?? "New register") }}</a>
             @endswitch
         </div>
     @endif
