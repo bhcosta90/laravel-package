e8f6da16b93d52260cf038cd961c4d625189337d
Bruno Costa <bhcosta90@gmail.com>
2025-03-08 12:55:07 -0300

    Add ContactFactory for generating fake contact data

---
+++ database/factories/ContactFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Database\Factories;
+
+use App\Models\Contact;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ContactFactory extends Factory
+{
+    protected $model = Contact::class;
+
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name,
+        ];
+    }
+}
