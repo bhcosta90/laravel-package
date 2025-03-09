f4fb7f02e53dc377877fab1c949aa867b1f49df5
Bruno Costa <bhcosta90@gmail.com>
2025-03-09 10:36:26 -0300

    Enhance AsServiceDestroyTrait to support before and after delete hooks

--- src/Service/Traits/AsServiceDestroyTrait.php
+++ src/Service/Traits/AsServiceDestroyTrait.php
@@ -12,7 +12,19 @@ trait AsServiceDestroyTrait
     public function destroy(Model $model): true
     {
         return DB::transaction(function () use ($model) {
+            $fieldId = $model->getKeyName();
+
+            $id = $model->{$fieldId};
+
+            if (method_exists($this, 'beforeDelete')) {
+                $this->beforeDelete($model);
+            }
+
             $model->delete();
+
+            if (method_exists($this, 'afterDelete')) {
+                $this->afterDelete($id);
+            }
 
             return true;
         });
