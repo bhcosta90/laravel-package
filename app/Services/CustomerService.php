872fc76ff1a4643c3d36f2dbe0c9e68d6c40db1c
Bruno Costa <bhcosta90@gmail.com>
2025-03-09 10:49:41 -0300

    Add lifecycle methods for before and after store, update, and delete in CustomerService

--- app/Services/CustomerService.php
+++ app/Services/CustomerService.php
@@ -13,4 +13,54 @@ class CustomerService extends BaseService
     {
         return Customer::class;
     }
+
+    public function beforeStore($attribute): array
+    {
+        $attribute['name'] .= ' - After Store';
+
+        return $attribute;
+    }
+
+    public function beforeUpdate($attribute): array
+    {
+        $attribute['name'] .= ' - Update Store';
+
+        return $attribute;
+    }
+
+    public function beforeSave($attribute): array
+    {
+        $attribute['name'] .= ' - After Save';
+
+        return $attribute;
+    }
+
+    public function afterStore(): void
+    {
+        $name = substr($this->getModel()->name, 0, -14);
+        $this->getModel()->update(compact('name'));
+    }
+
+    public function afterUpdate(): void
+    {
+        $name = substr($this->getModel()->name, 0, -15);
+        $this->getModel()->update(compact('name'));
+    }
+
+    public function afterSave(): void
+    {
+        $name = substr($this->getModel()->name, 0, -13);
+        $this->getModel()->update(compact('name'));
+    }
+
+    public function beforeDelete($model): void
+    {
+        $model->name .= ' - Before Delete';
+        $model->save();
+    }
+
+    public function afterDelete($id): void
+    {
+        //
+    }
 }
