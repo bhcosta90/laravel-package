c0d7c0c952d792a25aa1665fe321795c0f2430ba
Bruno Costa <bhcosta90@gmail.com>
2025-03-09 10:39:58 -0300

    Add file upload functionality with UploadFile and UploadFileOutput classes

---
+++ src/Service/Traits/Helper/AsModel.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace CodeFusion\Service\Traits\Helper;
+
+use Illuminate\Database\Eloquent\Model;
+
+trait AsModel
+{
+    protected Model $model;
+
+    public function setModel(Model $model): void
+    {
+        $this->model = $model;
+    }
+
+    public function getModel(): Model
+    {
+        return $this->model;
+    }
+}
