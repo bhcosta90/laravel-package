0480326d7da645f3b0d522aaa756eebba36be551
Bruno Costa <bhcosta90@gmail.com>
2025-03-09 10:36:29 -0300

    Enhance AsServiceStoreTrait to support before and after store hooks

--- src/Service/Traits/AsServiceStoreTrait.php
+++ src/Service/Traits/AsServiceStoreTrait.php
@@ -5,7 +5,7 @@ declare(strict_types = 1);
 namespace CodeFusion\Service\Traits;
 
 use CodeFusion\Service\Traits\Helper\BaseQueryTrait;
-use CodeFusion\Service\Traits\Helper\{SyncModelTrait};
+use CodeFusion\Service\Traits\Helper\{AsModel, SyncModelTrait};
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
 
@@ -13,11 +13,31 @@ trait AsServiceStoreTrait
 {
     use BaseQueryTrait;
     use SyncModelTrait;
+    use AsModel;
 
     public function store(array $attributes = []): Model
     {
+        if (method_exists($this, 'beforeStore')) {
+            $attributes = $this->beforeStore($attributes) + $attributes;
+        }
+
+        if (method_exists($this, 'beforeSave')) {
+            $attributes = $this->beforeSave($attributes) + $attributes;
+        }
+
         return DB::transaction(function () use ($attributes) {
             $callback = app($this->model())->create($attributes);
+
+            $this->setModel($callback);
+
+            if (method_exists($this, 'afterStore')) {
+                $this->afterStore($attributes);
+            }
+
+            if (method_exists($this, 'afterSave')) {
+                $this->afterSave($attributes);
+            }
+
             $this->syncModel($callback, $attributes);
 
             return $callback->refresh();
