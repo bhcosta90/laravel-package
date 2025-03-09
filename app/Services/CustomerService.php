b9827b74d5c1906646f02236d9477ca510b2ca90
Bruno Costa <bhcosta90@gmail.com>
2025-03-09 10:36:43 -0300

    Add beforeSave method to handle photo uploads in CustomerService

--- app/Services/CustomerService.php
+++ app/Services/CustomerService.php
@@ -6,11 +6,26 @@ namespace App\Services;
 
 use App\Models\Customer;
 use CodeFusion\Service\BaseService;
+use CodeFusion\Service\Support\UploadFile;
 
 class CustomerService extends BaseService
 {
     protected function model(): string
     {
         return Customer::class;
+    }
+
+    protected function beforeSave(array $data, ?Customer $customer = null): array
+    {
+        if (filled($data['photo'])) {
+            $uploadFile = new UploadFile();
+
+            $data['photo'] = $uploadFile->upload(
+                $data['photo'],
+                'customers/' . $customer?->id ?? str()->uuid()
+            )->path;
+        }
+
+        return $data;
     }
 }
