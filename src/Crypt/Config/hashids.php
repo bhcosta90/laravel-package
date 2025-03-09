9a0022f24b973013468d8d8a8bff4e63913c952a
Bruno Costa <bhcosta90@gmail.com>
2025-03-09 08:13:50 -0300

    Add configuration option to enable hashids in hashids.php

--- src/Crypt/Config/hashids.php
+++ src/Crypt/Config/hashids.php
@@ -3,9 +3,9 @@
 declare(strict_types = 1);
 
 return [
+    'enable'  => env('CRYPT_ENABLE', false),
     'default' => 'main',
-
-    'main' => [
+    'main'    => [
         'salt'     => env('HASHIDS_SALT', env('APP_KEY')),
         'length'   => env('HASHIDS_LENGTH', 10),
         'alphabet' => env('HASHIDS_ALPHABET', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'),
