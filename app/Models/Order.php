28ef2fb723fadc44f4c6dfc66fd6a6b93166e086
Bruno Costa <bhcosta90@gmail.com>
2025-03-09 09:44:19 -0300

    Add OrderFactory for creating order instances and include HasFactory trait in Order model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -5,11 +5,13 @@ declare(strict_types = 1);
 namespace App\Models;
 
 use CodeFusion\Crypt\Model\AsHashId;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
     use AsHashId;
+    use HasFactory;
 
     protected $fillable = [
         'customer_id',
