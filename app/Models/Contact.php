5f7198060cb15c044774a33b3cba9ae572b98f54
Bruno Costa <bhcosta90@gmail.com>
2025-03-09 12:57:37 -0300

    Add is_principal field to Contact model and implement scopePrincipal for filtering

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -6,8 +6,8 @@ namespace App\Models;
 
 use CodeFusion\Model\Traits\BaseModel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\{Builder, Model};
 
 class Contact extends Model
 {
@@ -16,11 +16,16 @@ class Contact extends Model
 
     protected $fillable = [
         'name',
-        'type',
+        'is_principal',
     ];
 
     public function customer(): BelongsTo
     {
         return $this->belongsTo(Customer::class);
+    }
+
+    public function scopePrincipal(Builder $builder): void
+    {
+        $builder->where('is_principal', true);
     }
 }
