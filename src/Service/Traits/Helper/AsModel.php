7ffc8292705f4c3d99916a43420b5e3e6d57f7cf
Bruno Costa <bhcosta90@gmail.com>
2025-03-09 11:03:55 -0300

    Update AsModel trait to allow nullable model property

--- src/Service/Traits/Helper/AsModel.php
+++ src/Service/Traits/Helper/AsModel.php
@@ -8,14 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 
 trait AsModel
 {
-    protected Model $model;
+    protected ?Model $model = null;
 
     public function setModel(Model $model): void
     {
         $this->model = $model;
     }
 
-    public function getModel(): Model
+    public function getModel(): ?Model
     {
         return $this->model;
     }
