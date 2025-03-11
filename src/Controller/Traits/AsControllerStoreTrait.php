10422979bb3b5e20187a9d4bbb2580b5fbbba228
Bruno Henrique da Costa <bruno.costa@mobiis.com.br>
2025-03-11 20:43:14 -0300

    Refactor AsControllerStoreTrait to use AsAddRequest for request handling

--- src/Controller/Traits/AsControllerStoreTrait.php
+++ src/Controller/Traits/AsControllerStoreTrait.php
@@ -4,19 +4,21 @@ declare(strict_types = 1);
 
 namespace CodeFusion\Controller\Traits;
 
+use CodeFusion\Controller\Traits\Helper\AsAddRequest;
+
 trait AsControllerStoreTrait
 {
+    use AsAddRequest;
+
     abstract protected function service(): string;
 
     abstract protected function resource(): string;
-
-    abstract protected function requestStore(): string;
 
     public function store()
     {
         $service  = app($this->service());
         $resource = $this->resource();
-        $request  = app($this->requestStore());
+        $request  = app($this->request()[__FUNCTION__]);
 
         $data   = $request->validated();
         $params = $request->route()?->parameters() ?: [];
