b511fac4d87346369aa737c8ad11ae1e7ac1a5f4
Bruno Henrique da Costa <bruno.costa@mobiis.com.br>
2025-04-15 13:04:16 -0300

    zerando o projeto

--- rector.php
+++ rector.php
@@ -6,7 +6,7 @@ use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()
     ->withPaths([
-        __DIR__ . '/src',
+        __DIR__.'/_bkp_src',
         __DIR__ . '/tests',
     ])
     ->withPreparedSets(
